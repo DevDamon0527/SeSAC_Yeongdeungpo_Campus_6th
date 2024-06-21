ad13efd7430878c30a6345c1ca22545ac85bcf0d
sean-spreatics <sean@spreatics.com>
2024-06-21 18:50:13 +0900

    study: sql

---
+++ code/lecture/14-mysql/index.sql
@@ -0,0 +1,23 @@
+-- 데이터베이스: 데이터의 집합
+-- DBMS: Database Management System, 데이터베이스를 운영/관리하는 프로그램
+	-- ex. MySQL
+-- SQL: 데이터베이스를 구축, 관리하고 활용하기 위해 사용하는 언어
+-- 참고) 스키마 == 데이터베이스
+
+-- DDL (Data Definition Language)
+-- : 데이터베이스, 테이블을 "정의"하는 언어
+
+-- 1. 데이터베이스 생성
+create database codingon character set utf8mb4 collate utf8mb4_unicode_ci;
+
+-- 2. 데이터베이스 목록
+show databases;
+
+-- 3. 데이터베이스 사용 선언
+use codingon;
+
+
+
+
+
+
