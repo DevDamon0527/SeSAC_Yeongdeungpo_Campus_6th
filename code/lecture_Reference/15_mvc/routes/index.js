4ce51f0e33dd4d6eb124695681f4c50056003e4d
sean-spreatics <sean@spreatics.com>
2024-06-24 16:15:58 +0900

    study: MVC 패턴

--- code/lecture_Reference/15_mvc/routes/index.js
+++ code/lecture_Reference/15_mvc/routes/index.js
@@ -52,7 +52,74 @@ router.get('/comment/:id', (req, res) => {
   // req.params: 라우트 매개변수 정보 확인가능
   // :id인 경우 req.params.id 로 조회 가능
   console.log(req.params); // { id: 'n' }
-  console.log(req.params.id); // n
+  console.log(req.paraconst express = require('express');
+const router = express.Router();
+
+// (임시) DB에서 가지고온 데이터
+const comments = [
+    {
+      id: 1,
+      userid: 'helloworld',
+      date: '2022-10-31',
+      comment: '안녕하세요^~^',
+    },
+    {
+      id: 2,
+      userid: 'happy',
+      date: '2022-11-01',
+      comment: '반가워유',
+    },
+    {
+      id: 3,
+      userid: 'lucky',
+      date: '2022-11-02',
+      comment: '오 신기하군',
+    },
+    {
+      id: 4,
+      userid: 'bestpart',
+      date: '2022-11-02',
+      comment: '첫 댓글입니당ㅎㅎ',
+    },
+];
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.get('/comments', (req, res) => {
+    res.render('comments', { comments }); // { comments: comments }
+});
+
+// 콜론(:): 요청의 주소에서 "변수"를 사용하는 방법
+router.get('/comment/:id', (req, res) => {
+    console.log(req.params); // { id: '1' } 
+    console.log(req.params.id) // '1' 
+
+    // 댓글 id: 요청 주소로 들어온 매개변수 (:id)
+    const commentId = req.params.id; 
+    console.log(comments[commentId - 1]) 
+
+    // 존재하지 않는 id에 대해서 요청할 때, 404 처리
+    if (!comments[commentId - 1]) { // !undefined => true
+        return res.render('404');
+    }
+
+    res.render('comment', 
+        { comment: comments[commentId - 1] });
+});
+
+// 요청의 주소에 변수 여러개 사용 예시
+router.get('/test/:id/:name', (req, res) => {
+    // {
+    //     "id": "1",
+    //     "name": "banana"
+    //  }
+    
+    res.send(req.params);
+});
+
+module.exports = router;ms.id); // n
 
   const commentId = req.params.id; // comment id가 1이라면
   console.log(comments[commentId - 1]); // 배열의 0번째 요소
