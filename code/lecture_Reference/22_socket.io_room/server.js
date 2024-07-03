26bb49b8632261f016c6e8a9ae4022ad4bb1bbb7
sean-spreatics <sean@spreatics.com>
2024-07-03 15:32:32 +0900

    수업 코드

--- code/lecture_Reference/22_socket.io_room/server.js
+++ code/lecture_Reference/22_socket.io_room/server.js
@@ -24,25 +24,39 @@ app.get('/', (req, res) => {
 // 사용자 B (socket.id: "5678", userName: "Bob"))
 
 function getUserList(room) {
-     //room에 접속한 모든 사용자 정보를 저장할 배열 초기화.
-     const users = [];
+    //room에 접속한 모든 사용자 정보를 저장할 배열 초기화.
+    const users = [];
 
-     //room는 접속한 룸 id
-     //특정 채팅방에 접속한 socket.id 집합 값을 찾음
-     const clients = io.sockets.adapter.rooms.get(room);  // Set { "1234", "5678" }
-    // console.log("io.sockets > " , io.sockets);
-    // console.log("io.sockets.adapter > " , io.sockets.adapter);
-    // console.log("io.sockets.adapter.rooms > " , io.sockets.adapter.rooms);
-    // io.sockets = 연결된 모든 소켓을 관리하는 객체
-    // adapter : 소켓 서버 간의 상태를 관리하는 역할
-    // rooms : 모든 방(room) 정보를 저장하고 있는 객체
-    // get(room) : 특정 방(room)에 연결된 소켓 ID를 Set형태로 반환.
+    //room는 접속한 룸 id
+    //특정 채팅방에 접속한 socket.id 집합 값을 찾음
+    const clients = io.sockets.adapter.rooms.get(room);  // Set { "1234", "5678" }
+
+    // console.log("io.sockets >> " , io.sockets);
+    // console.log("io.sockets.adapter >> " , io.sockets.adapter);
+    // console.log("io.sockets.adapter.rooms >> " , io.sockets.adapter.rooms);
+
+    // 'io'객체
+    // - Socket.IO 인스턴스를 나타내는 객체 :: 전체 소켓 서버를 관리. 
+
+    // 'io.sockets"
+    // - 현재 연결된 모든 소켓들을 관리하는 객체 :: 소켓들이 상태를 관리하고 다양한 소켓 관련 작업 수행.
+
+    // 'io.sockets.adapter'
+    // - 소켓 서버 간의 상태를 관리하는 역할을 담당하는 객체 :: 소켓들읜 연결 상태와 방(room) 정보를 관리.                                                                                              
+
+    // 'io.sockets.adapter.room'
+    // - 모든 방(room) 정보를 저장하고 있는 객체 :: 각 방에 접속한 클라이언트들의 정보를 포함.
+
+    // 'get(room)'
+    // - 위 객체에서 특정 방(room) ID에 해당하는 정보를 가져오는 역할 :: 해당 방에 접속한 클라이언트들의 소켓 ID 들을 Set형태로 반환.
+
 
     // 방에 클라이언트가 있는 경우에 실행!
     if (clients) {
         // 각 socket.ID에 대해 반복 실행
         clients.forEach((client) => {
             // console.log(client);
+
             // socket.ID로 소켓 객체 가져오기.
             // io.sockets.sockets: socket.id가 할당한 변수들의 객체값
             const userSocket = io.sockets.sockets.get(client);
@@ -53,7 +67,6 @@ function getUserList(room) {
             
             // 사용자 정보를 배열에 추가
             users.push(info);
-            
         });
     }
     return users; // users = [{ userName: "Alice", key: "1234" }, { userName: "Bob", key: "5678" }]
