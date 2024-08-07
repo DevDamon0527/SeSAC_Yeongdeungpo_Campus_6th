b14d1162493f292b25d24f2ce12ace3157223d93
sean-spreatics <sean@spreatics.com>
2024-08-07 19:17:40 +0900

    study: 0807

--- code/lecture/26-event/src/App.js
+++ code/lecture/26-event/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import ClassBind from './ClassBind';
 import Counter from './Counter';
+import EventStudy from './EventStudy';
 import Prac from './Prac';
+import Prac2 from './Prac2';
 import SyntheticEvent from './SyntheticEvent';
 
 function App() {
   return (
     <div className="App">
+      {/* <EventStudy/>
+      <hr />
+
       <SyntheticEvent/>
       <hr />
 
@@ -14,6 +19,9 @@ function App() {
       <hr />
 
       <Counter/>
+      <hr /> */}
+      
+      <Prac2 />
       <hr />
 
       <Prac/>
