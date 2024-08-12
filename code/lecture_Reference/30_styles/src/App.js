f8b3c098add6af0617b2014d0e9d634d381b7402
Damon0527 <damon@spreatics.com>
2024-08-12 11:21:48 +0900

    lecture ready(0810) : react_styles_pptx(2)

--- code/lecture_Reference/30_styles/src/App.js
+++ code/lecture_Reference/30_styles/src/App.js
@@ -1,15 +1,23 @@
 import CssModuleComponents from './CssModuleComponents';
-import './styles/App.css'
+import SassComponent from './SassComponent';
+import StyledComponent from './StyledComponent';
+import './styles/App.css';
 function App() {
-  return (
-    <div className="App">
-      <h1>React Styling</h1>
-      <hr />
+    return (
+        <div className="App">
+            <h1>React Styling</h1>
+            <hr />
 
-      <h2>CSS Module</h2>
-      <CssModuleComponents/>
-    </div>
-  );
+            <h2>CSS Module</h2>
+            <CssModuleComponents />
+
+            <h2>SASS</h2>
+            <SassComponent />
+
+            <h2>Styled-Components</h2>
+            <StyledComponent />
+        </div>
+    );
 }
 
 export default App;
