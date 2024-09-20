2e4a73cfbcea512394f4bf2a8da0a9db75beae36
Damon0527 <damon@spreatics.com>
2024-09-20 18:16:47 +0900

    study : jwt, java 설치 (0920)

--- code/lecture_Reference/18-session-cookie-demo/app.js
+++ code/lecture_Reference/18-session-cookie-demo/app.js
@@ -10,62 +10,67 @@ const userInfo = { id: 'banana', pw: '1234' };
 // 미들웨어 설정
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(session({
-  secret: 'your_secret_key',
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+    session({
+        secret: 'your_secret_key',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
 
 // 뷰 엔진 설정
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
 // 라우트
 app.get('/', (req, res) => {
-  if (req.session.loggedin) {
-    res.redirect('/dashboard');
-  } else {
-    res.render('login', { rememberedId: req.cookies.rememberedId || '' });
-  }
+    if (req.session.loggedin) {
+        res.redirect('/dashboard');
+    } else {
+        res.render('login', { rememberedId: req.cookies.rememberedId || '' });
+    }
 });
 
 app.post('/login', (req, res) => {
     const { id, pw, remember } = req.body;
-    
+
     if (id === userInfo.id && pw === userInfo.pw) {
-      req.session.loggedin = true;
-      req.session.userId = id;
-  
-      if (remember === '1') {
-        res.cookie('rememberedId', id, { maxAge: 30 * 24 * 60 * 60 * 1000 }); // 30일
-      } else {
-        res.clearCookie('rememberedId');
-      }
-  
-      res.redirect('/dashboard');
+        console.log(req.session);
+
+        req.session.loggedin = true;
+        req.session.userId = id;
+        console.log(req.session.userId);
+        if (remember === '1') {
+            res.cookie('rememberedId', id, {
+                maxAge: 30 * 24 * 60 * 60 * 1000,
+            }); // 30일
+        } else {
+            res.clearCookie('rememberedId');
+        }
+
+        res.redirect('/dashboard');
     } else {
-      res.send('로그인 실패. <a href="/">다시 시도</a>');
+        res.send('로그인 실패. <a href="/">다시 시도</a>');
     }
-  });
+});
 
 app.get('/dashboard', (req, res) => {
-  if (req.session.loggedin) {
-    res.render('dashboard', { userId: req.session.userId });
-  } else {
-    res.redirect('/');
-  }
+    if (req.session.loggedin) {
+        res.render('dashboard', { userId: req.session.userId });
+    } else {
+        res.redirect('/');
+    }
 });
 
 app.get('/logout', (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      console.log(err);
-    }
-    res.redirect('/');
-  });
+    req.session.destroy((err) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
 });
 
 app.listen(port, () => {
-  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
+    console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
